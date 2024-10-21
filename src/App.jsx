59235fa94e27db6d472d4f934ac6dbd99d1aecec
walid hasan <walid.official8@gmail.com>
2024-10-21 23:16:40 +0600

    project is ready to start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="">
+        <h1>Knowledge Cafe</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
